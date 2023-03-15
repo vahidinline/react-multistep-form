fb00653c237817ded6d0a0b0228c594f1fe49677
vahid Afshari <vahidinline@gmail.com>
2023-03-15 14:46:45 +0200

    add submittion message

--- src/components/Forms/Success/Success.js
+++ src/components/Forms/Success/Success.js
@@ -5,6 +5,7 @@ import { FormContext } from '../../../App';
 function Success() {
   const { formData } = useContext(FormContext);
   const [isLoading, setIsLoading] = useState(false);
+  const [result, setResult] = useState(null);
   const handleSubmit = async () => {
     setIsLoading(true);
     const result = await axios.post(
@@ -14,29 +15,39 @@ function Success() {
       }
     );
     console.log(result);
+    setResult(result.request.statusText);
+    console.log(result.request.statusText);
     setIsLoading(false);
   };
 
   return (
     <div className="font-medium">
-      <p className="text-lg text-center">
-        Dear {formData.name}; please click on the submit button to complete your
-        registration
-      </p>
-      <p className="text-lg text-center">
-        {formData.numOfParticipants}{' '}
-        {formData.diet === 'vegan' ? 'Vegan' : 'Not Vegan'} people will
-        participate in the event
-      </p>
+      {result === 'OK' ? (
+        <p className="text-lg text-center text-green-500">
+          Your registration was successful
+        </p>
+      ) : (
+        <div>
+          <p className="text-lg text-center">
+            Dear {formData.name}; please click on the submit button to complete
+            your registration
+          </p>
+          <p className="text-lg text-center">
+            {formData.numOfParticipants}{' '}
+            {formData.diet === 'vegan' ? 'Vegan' : 'Not Vegan'} people will
+            participate in the event
+          </p>
 
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 w-full"
-        onClick={handleSubmit}>
-        <div role="status">
-          <span class="sr-only">{isLoading ? 'Loading...' : 'Submit'}</span>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 w-full"
+            onClick={handleSubmit}>
+            <div role="status">
+              <span class="sr-only">{isLoading ? 'Loading...' : 'Submit'}</span>
+            </div>
+            Submit
+          </button>
         </div>
-        Submit
-      </button>
+      )}
     </div>
   );
 }
